80d2d5e3e0e88b3b84b8c4b867382527981c6d41
mimooo <maryamtb.17@gmail.com>
2020-08-01 15:57:27 +0100

    update

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,8 +1,16 @@
 const request = require('request')
+const { jsdom } = require("jsdom");
+
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/0f7df2b6c36478f46d087e3956a10717/' + latitude + ',' + longitude
+    
+    let jsdom;
 
+    function darkmode() {
+        // var element = jsdom.window.document.body;
+        element.classList.toggle("dark-mode");
+    }
 
     request({ url, json: true }, (error, { body }) => {
 
@@ -13,9 +21,12 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
+        } else if (body.currently.temperature <= 60) {
+            darkmode()
         } else {
             callback(undefined, 'Daily Report: ' + body.daily.data[0].summary + ' It is currently ' + valueCelsius + '°C (' + body.currently.temperature + '°F)' + ' out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
+        
     })
 }
 
